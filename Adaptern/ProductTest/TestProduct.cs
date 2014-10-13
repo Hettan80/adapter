dbac075df3819d40ff2ae438226615732fecedd0
Hettan80 <henrietta.lodin@gmail.com>
2014-10-13 13:55:44 +0200

    fix o trix

--- Adaptern/ProductTest/TestProduct.cs
+++ Adaptern/ProductTest/TestProduct.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace SomeExternalVendorProductTest
+namespace ProductTest
 {
    [TestClass]
-   public class UnitTest1
+   public class TestProduct
    {
       [TestMethod]
       public void TestMethod1()
